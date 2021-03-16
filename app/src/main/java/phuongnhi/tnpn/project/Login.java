24bf0eabc52cedb14c74577e29e24877d173fb4d
Trần Ngọc Phương Nhi <tranngocphuongnhi0412@gmail.com>
2021-03-16 11:38:06 +0700

    thêm giao diện đăng ký và thư viện room database

--- app/src/main/java/phuongnhi/tnpn/project/Login.java
+++ app/src/main/java/phuongnhi/tnpn/project/Login.java
@@ -7,21 +7,36 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
     Button btnLogin;
+    EditText account, password;
+    TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        account = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        register = (TextView) findViewById(R.id.dangKy);
         btnLogin = (Button) findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Login.this, TeacherHome.class);
+                startActivity(intent);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Register.class);
                 startActivity(intent);
             }
         });
