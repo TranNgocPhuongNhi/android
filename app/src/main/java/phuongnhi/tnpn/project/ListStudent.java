6683b96912c0d060d32898d588d46512392f887e
giahuy2808 <johnkerryxxx@gmail.com>
2021-04-30 18:59:01 +0700

    notification 1
    gởi dc text rồi

--- app/src/main/java/phuongnhi/tnpn/project/ListStudent.java
+++ app/src/main/java/phuongnhi/tnpn/project/ListStudent.java
@@ -26,6 +26,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import java.util.ArrayList;
 import java.util.Map;
@@ -57,6 +58,8 @@ public class ListStudent extends AppCompatActivity {
         userID  = user.getUid();
 
         data = new ArrayList<MyStudent>();
+
+        FirebaseMessaging.getInstance().subscribeToTopic("all");
 
         btnBack = findViewById(R.id.imageView5);
         btnAdd = findViewById(R.id.imageView6);
@@ -128,6 +131,19 @@ public class ListStudent extends AppCompatActivity {
             btnCancel.setOnClickListener(v1 -> {
                 myRefAttendance.child("Date: " + getDayNow()).child("CodeNow").removeValue();
                 alertDialog.dismiss();
+
+
+                String title = "Thông báo vắng học";
+                String msg = "Bạn đã bị cấm thi";
+                //send notification
+                FcmNotificationsSender notificationsSender = new FcmNotificationsSender(
+                        "/topics/all",
+                        title,
+                        msg,
+                        getApplicationContext(),
+                        ListStudent.this);
+
+                notificationsSender.SendNotifications();
             });
             btnSave.setOnClickListener(v12 -> {
                 for(MyStudent myStudent: data){
